b805d8a4e94493765f3c9c723266adb0464e6e21
Damian Cyrana <damiancyrana@gmail.com>
2023-06-08 10:36:20 +0200

    Implemented connection to Azure IoT

--- maceration-driver.py
+++ maceration-driver.py
@@ -1,110 +1,127 @@
 import time
-from datetime import datetime, timedelta
-
+import json
 from RPLCD.i2c import CharLCD
 from gpiozero import OutputDevice
 from w1thermsensor import W1ThermSensor
+from datetime import datetime, timedelta
+from azure.iot.device import IoTHubDeviceClient
+
+
+class AzureIoTConnect:
+    def __init__(self, config_file):
+        self.connection_string = self.load_connection_string(config_file)
+        self.client = self.initialize_client()
+
+    def load_connection_string(self, config_file):
+        try:
+            with open(config_file, "r") as file:
+                config = json.load(file)
+                return config["IoTHubConnectionString"]
+        except FileNotFoundError:
+            print("Configuration file not found")
+            exit()
+        except KeyError:
+            print("ConnectionString not found in the configuration file")
+            exit()
+
+    def initialize_client(self):
+        try:
+            client = IoTHubDeviceClient.create_from_connection_string(self.connection_string)
+            return client
+        except:
+            print("Failed to initialize Azure IoT client")
+            exit()
+
+    def send_message(self, message):
+        try:
+            self.client.send_message(json.dumps(message))
+        except:
+            print("Failed to send message to Azure IoT Hub")
+
+    @property
+    def is_connected(self):
+        return self.client.connected
 
 
 class WineMacerator:
-    """A class to manage a wine macerator using a temperature sensor, an LCD display and a relay."""
-    
-    LCD_ADDR = 0x27
-    RELAY_DURATION_MIN = 1
-    MIXTURE_INTERVAL_HR = 0.25
-    TEMP_UPDATE_INTERVAL_MIN = 1
+    LCD_I2C_ADDRESS = 0x27
+    RELAY_DURATION_MINUTES = 1
+    MIXTURE_INTERVAL_HOURS = 0.25
+    TEMP_UPDATE_INTERVAL_MINUTES = 1
+    MACERATION_DURATION_DAYS = 14
 
     def __init__(self):
-        """Initializes the macerator components and sets the initial times."""
+        self.azure_iot = AzureIoTConnect("config.json")
         self.initialize_components()
         self.set_initial_times()
 
-
     def initialize_components(self):
-        """Initializes the LCD, the temperature sensor and the relay."""
-        self.lcd = CharLCD(i2c_expander='PCF8574', address=self.LCD_ADDR, port=1, cols=16, rows=2, dotsize=8)
+        self.lcd = CharLCD(i2c_expander='PCF8574', address=self.LCD_I2C_ADDRESS, port=1, cols=16, rows=2, dotsize=8)
         self.sensor = W1ThermSensor()
         self.relay = OutputDevice(26, active_high=False)
 
-
     def set_initial_times(self):
-        """Sets the initial times for the next temperature update, the next relay activation, and the next second update."""
         self.relay_end_time = None
         self.next_temp_update = datetime.now()
         self.next_relay_activation = datetime.now()
         self.next_second_update = datetime.now()
-
+        self.end_maceration_time = datetime.now() + timedelta(days=self.MACERATION_DURATION_DAYS)
 
     def update_temperature(self):
-        """Updates the temperature displayed on the LCD."""
         self.set_next_temp_update_time()
         temperature = self.get_temperature_from_sensor()
-        self.display_temperature_on_lcd(temperature)
-
+        self.display_info_on_lcd(temperature)
+        self.azure_iot.send_message({"wine_temp": temperature})
 
     def set_next_temp_update_time(self):
-        """Sets the next temperature update time."""
-        self.next_temp_update = datetime.now() + timedelta(minutes=self.TEMP_UPDATE_INTERVAL_MIN)
-
+        self.next_temp_update = datetime.now() + timedelta(minutes=self.TEMP_UPDATE_INTERVAL_MINUTES)
 
     def get_temperature_from_sensor(self):
-        """Returns the temperature read from the sensor."""
         return self.sensor.get_temperature()
 
-
-    def display_temperature_on_lcd(self, temperature):
-        """Displays the given temperature on the LCD."""
+    def display_info_on_lcd(self, temperature):
+        remaining_days = (self.end_maceration_time - datetime.now()).days
         self.lcd.cursor_pos = (0, 0)
-        self.lcd.write_string(f'Wine T: {temperature:.1f} C'.ljust(16))
+        self.lcd.write_string(f'T:{temperature:.1f} D:{remaining_days}'.ljust(11))
+        self.display_azure_connection_status()
 
+    def display_azure_connection_status(self):
+        self.lcd.cursor_pos = (0, 12)
+        self.lcd.write_string("OK-A" if self.azure_iot.is_connected else "NO-A")
 
     def activate_relay(self):
-        """Activates the relay and updates the LCD to show that blending has started."""
         self.relay.on()
         self.set_relay_end_time()
         self.display_blending_started_on_lcd()
 
-
     def set_relay_end_time(self):
-        """Sets the relay end time."""
-        self.relay_end_time = datetime.now() + timedelta(minutes=self.RELAY_DURATION_MIN)
-
+        self.relay_end_time = datetime.now() + timedelta(minutes=self.RELAY_DURATION_MINUTES)
 
     def display_blending_started_on_lcd(self):
-        """Displays on the LCD that blending has started."""
         self.lcd.cursor_pos = (1, 0)
         self.lcd.write_string('Mieszanie: --:--'.ljust(16))
 
-
     def deactivate_relay(self):
-        """Deactivates the relay."""
         self.relay.off()
         self.set_next_relay_activation_time()
 
-
     def set_next_relay_activation_time(self):
-        """Sets the next relay activation time."""
-        self.next_relay_activation = datetime.now() + timedelta(hours=self.MIXTURE_INTERVAL_HR)
-
+        self.next_relay_activation = datetime.now() + timedelta(hours=self.MIXTURE_INTERVAL_HOURS)
 
     def update_relay_timer(self):
-        """Updates the relay timer displayed on the LCD."""
         self.lcd.cursor_pos = (1, 0)
         remaining = (self.relay_end_time if self.relay.value else self.next_relay_activation) - datetime.now()
         self.display_time_on_lcd(remaining, self.relay.value)
 
-
     def display_time_on_lcd(self, remaining, relay_active):
-        """Displays the given remaining time on the LCD, depending on whether the relay is active or not."""
         if relay_active:
-            self.lcd.write_string(f'Mieszanie: {int(remaining.total_seconds() / 60)}:{int(remaining.total_seconds() % 60):02d}'.ljust(16))
+            self.lcd.write_string(f'Blend: {int(remaining.total_seconds() / 60)}:{int(remaining.total_seconds() % 60):02d}'.ljust(16))
         else:
-            self.lcd.write_string(f'Nast.za: {int(remaining.total_seconds() / 3600)}:{int(remaining.total_seconds() % 3600 / 60):02d}:{int(remaining.total_seconds() % 60):02d}'.ljust(16))
-
+            self.lcd.write_string(f'Next: {int(remaining.total_seconds() / 3600)}:{int(remaining.total_seconds() % 3600 / 60):02d}:{int(remaining.total_seconds() % 60):02d}'.ljust(16))
 
     def run(self):
-        """Runs the macerator, periodically updating the temperature, activating and deactivating the relay, and updating the relay timer."""
-        while True:
+        self.display_azure_connection_status()
+        while datetime.now() < self.end_maceration_time:
             current_time = datetime.now()
 
             if current_time >= self.next_temp_update:
@@ -122,8 +139,13 @@ class WineMacerator:
             next_event_time = min(self.next_temp_update, self.next_second_update, self.relay_end_time if self.relay.value else self.next_relay_activation)
             time.sleep(max(0, (next_event_time - current_time).total_seconds()))
 
+        self.lcd.clear()
+        self.lcd.write_string("Zakonczono proces")
+
 
 if __name__ == "__main__":
-    macerator = WineMacerator()
-    macerator.run()
-    
+    try:
+        macerator = WineMacerator()
+        macerator.run()
+    except Exception as e:
+        print(f"An error occurred: {e}")
